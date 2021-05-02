0823391d616ee3e1be70b583f47cbb1fc721b1ab
funny family <maksvadik228@gmail.com>
2021-05-02 19:55:22 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import express from 'express';
+import { Server } from 'http';
+
+// import io from 'socket.io'
+
+import dotenv from 'dotenv';
+dotenv.config();
 
 import { chatApiV1 } from './modules/chat.module';
 
 const app = express();
+const server = new Server(app);
 const port = 8000;
 
 app.use('/api/v1', chatApiV1);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log('\x1b[36m', `Server running at: http://localhost:${port}/`);
 });
