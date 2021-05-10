7c433acf235ed39145ba5ab128a9357b0da7d4bb
funny family <maksvadik228@gmail.com>
2021-05-10 18:28:31 +0700

    update main file

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,25 @@
 import express from 'express';
 import { Server } from 'http';
 
-// import io from 'socket.io'
-
 import dotenv from 'dotenv';
 dotenv.config();
 
+// import { handleMongooseErrors } from './middlewares/mongoose-errors-handler.middleware';
+
 import { chatApiV1 } from './modules/chat.module';
+import { userApiV1 } from './modules/user.module';
 
 const app = express();
 const server = new Server(app);
-const port = 8000;
+const port = process.env.PORT || 8800;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(handleMongooseErrors);
 
 app.use('/api/v1', chatApiV1);
+app.use('/api/v1', userApiV1);
 
 server.listen(port, () => {
   console.log('\x1b[36m', `Server running at: http://localhost:${port}/`);
