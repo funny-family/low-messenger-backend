8473f22bf983732464d3ab2910a85d3ea74e9abb
funny family <maksvadik228@gmail.com>
2021-05-13 22:15:56 +0700

    update main file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import responseTime from 'response-time';
+// https://stackoverflow.com/questions/10183291/how-to-get-the-full-url-in-express
 import { Server } from 'http';
 
 import dotenv from 'dotenv';
 dotenv.config();
 
-// import { handleMongooseErrors } from './middlewares/mongoose-errors-handler.middleware';
+import { passport } from './middlewares/passport.middleware';
 
 import { chatApiV1 } from './modules/chat.module';
 import { userApiV1 } from './modules/user.module';
@@ -13,8 +15,11 @@ const app = express();
 const server = new Server(app);
 const port = process.env.PORT || 8800;
 
+app.use(responseTime());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // app.use(handleMongooseErrors);
 
@@ -23,4 +28,5 @@ app.use('/api/v1', userApiV1);
 
 server.listen(port, () => {
   console.log('\x1b[36m', `Server running at: http://localhost:${port}/`);
+  // console.log(1213213);
 });
