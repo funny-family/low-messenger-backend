250cd9c87a3b78f0555586029e489afddb1bde2d
funny family <maksvadik228@gmail.com>
2021-05-25 21:15:22 +0700

    update chat model

--- src/modules/chat.module.ts
+++ src/modules/chat.module.ts
@@ -1,14 +1,11 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import rateLimit, { RateLimit } from 'express-rate-limit';
 
-import { passport } from '../middlewares/passport.middleware';
-// import { handleMongooseErrors } from '../middlewares/mongoose-errors-handler.middleware'
+import { ChatModel } from '../database/models/chat.model';
 
-import { ChatModel, IChatSchema } from '../database/models/chat.model';
-
+import { ResponseObject } from '../utils/api';
 import { ErrorObjectConstructor } from '../utils/error-object-constructor.class';
-
-import axios from 'axios';
+import { checkIfStringIsEmpty } from '../utils/check-if-string-is-empty.function';
 
 export const chatApiV1 = Router();
 
@@ -18,117 +15,371 @@ const createChatRouteRateLimiter: RateLimit = rateLimit({
   message: 'Too many requests from this IP, please try again latter!'
 });
 chatApiV1.post(
-  '/create-chat',
+  '/chat',
   // handleMongooseErrors,
   createChatRouteRateLimiter,
-  (req: Request, res: Response) => {
-    // const chatModel = new ChatModel({
-    //   name: req.body.name,
-    //   owner_id: req.body.ownerId,
-    //   password: req.body.password,
-    //   password_confirmation: req.body.passwordConfirmation,
-    //   user_IDs: req.body.userIDs
-    // });
+  async (req: Request, res: Response) => {
+    const headers = {
+      'Content-Type': 'application/json; charset=utf-8'
+    };
 
-    const chatModel = new ChatModel({
-      name: req.body.name,
-      owner_id: req.body.owner_id,
-      password: req.body.password,
-      password_confirmation: req.body.password_confirmation,
-      user_IDs: req.body.user_IDs
-    });
-
-    chatModel
-      .save()
-      .then((document) => {
-        const response = document.toJSON();
-        res.json(response).status(201);
-      })
-      .catch((error) => {
-        res.json(error).status(500);
+    try {
+      const chatModel = new ChatModel({
+        name: req.body.name,
+        owner_id: req.body.owner_id,
+        password: req.body.password,
+        password_confirmation: req.body.password_confirmation,
+        user_IDs: req.body.user_IDs
       });
+
+      const chatData = await chatModel.save();
+
+      const statusCode = 201;
+      const savedChatData = chatData.toJSON();
+      const responseData = new ResponseObject({
+        data: {
+          title: 'Created new user.',
+          attributes: {
+            ...savedChatData
+          }
+        },
+        errors: {}
+      });
+
+      res.set(headers);
+      res.status(statusCode);
+      res.json(responseData);
+
+      return;
+    } catch (error) {
+      try {
+        const errorObject = {
+          ...error.errors,
+          message: error.message
+        };
+
+        const statusCode = 400;
+        const responseData = new ResponseObject({
+          data: {},
+          errors: {
+            ...errorObject,
+            status: statusCode,
+            title: 'Cannot create user!'
+          }
+        });
+
+        res.set(headers);
+        res.status(statusCode);
+        res.json(responseData);
+
+        return;
+      } catch (error) {
+        const errorObject = {
+          ...error.errors,
+          message: error.message
+        };
+
+        const statusCode = 500;
+        const responseData = new ResponseObject({
+          data: {},
+          errors: {
+            ...errorObject,
+            status: statusCode,
+            title: 'Something went wrong on our side!'
+          }
+        });
+
+        res.set(headers);
+        res.status(statusCode);
+        res.json(responseData);
+
+        return;
+      }
+    }
   }
 );
 
-chatApiV1.post('/find-chat-by-entry-key/:entry_key', async (req: Request, res: Response) => {
-  try {
-    const chatEntryKey = req.params.entry_key;
-    const chat = await ChatModel.findOne({ entry_key: chatEntryKey }).exec();
-    const wasChatFound = chat;
+chatApiV1.delete(
+  '/chat/:entry_key',
+  // ratelimiter
+  async (req: Request, res: Response) => {
+    const headers = {
+      'Content-Type': 'application/json; charset=utf-8'
+    };
 
-    if (!wasChatFound) {
-      const errorObject = new ErrorObjectConstructor({
-        field: 'entry_key',
-        message: 'Chat not found!'
-      });
-
-      res.json(errorObject).status(404);
-    }
-
-    const responseData = chat?.toJSON();
-    // // @ts-ignore
-    // delete responseData?.user_IDs;
-    // // @ts-ignore
-    // delete responseData?.name;
-    // // @ts-ignore
-    // delete responseData?.owner_id;
-    // // @ts-ignore
-    // delete responseData?.createdAt;
-    // // @ts-ignore
-    // delete responseData?.updatedAt;
-
-    res.json(responseData).status(200);
-  } catch (error) {
-    res.json(error).status(500);
-  }
-});
-
-/**
- * watch this!:
- * https://github.com/koalex/node-lesson-messages
- * https://github.com/koalex/node-lesson-messages/blob/master/models/message.js
- *
- * https://github.com/koalex/vki-ngu-node
- * https://github.com/koalex/vki-ngu-node/tree/master/modules/chat
- */
-const joinChatRouteLimiter: RateLimit = rateLimit({
-  windowMs: 5 * 6 * 1000, // 5min
-  max: 4,
-  message: 'Too many requests from this IP, please try again latter!'
-});
-chatApiV1.post(
-  '/join-chat',
-  // joinChatRouteLimiter,
-  async (req: Request, res: Response, next: NextFunction) => {
     try {
-      const chatEnterKey = req.body.entry_key;
+      const entryKey = req.params.entry_key;
+      const chat = await ChatModel.findOneAndDelete({ entry_key: entryKey });
 
-      const fetchedChatData = await axios({
-        method: 'POST',
-        url: `http://localhost:8800/api/v1/find-chat-by-entry-key/${chatEnterKey}`
-      });
-
-      const chat: IChatSchema = fetchedChatData.data;
-      const doesChatHaveAPassword = chat.has_password;
-
-      if (!doesChatHaveAPassword) {
-        return res.json(chat).status(200);
+      if (!chat) {
+        throw new Error('Chat not found!');
       }
 
-      passport.authenticate(
-        'local',
-        { session: false, failWithError: true },
-        (err, options, info) => {
-          if (err) {
-            res.json(err).status(401);
+      const statusCode = 204;
+
+      res.set(headers);
+      res.status(statusCode);
+      res.end();
+
+      return;
+    } catch (error) {
+      try {
+        const errorObject = {
+          ...error.errors,
+          message: error.message
+        };
+
+        const statusCode = 404;
+        const responseData = new ResponseObject({
+          data: {},
+          errors: {
+            ...errorObject,
+            status: statusCode,
+            title: 'Mongo error!'
+          }
+        });
+
+        res.set(headers);
+        res.status(statusCode);
+        res.json(responseData);
+
+        return;
+      } catch (error) {
+        const statusCode = 500;
+        const responseData = new ResponseObject({
+          data: {},
+          errors: {
+            ...error,
+            status: statusCode,
+            title: 'Something went wrong on our side!'
+          }
+        });
+
+        res.set(headers);
+        res.status(statusCode);
+        res.json(responseData);
+
+        return;
+      }
+    }
+  }
+);
+
+chatApiV1.post(
+  '/find-by-entry-key/:entry_key',
+  // ratelimiter
+  async (req: Request, res: Response) => {
+    try {
+      const entryKey = await req.params.entry_key;
+      const isEntryKeyEmpty = entryKey.length === 0;
+
+      if (isEntryKeyEmpty) {
+        const responseData = 'Entry key is required!';
+
+        res.status(400).json(responseData);
+        return;
+      }
+
+      const chat = await ChatModel.findOne({ entry_key: entryKey }).exec();
+      const wasFound = !!chat;
+
+      const responseData = {
+        was_found: wasFound,
+        has_password: chat?.has_password
+      };
+
+      if (!wasFound) {
+        res.status(404).json(responseData);
+        return;
+      }
+
+      res.status(200).json(responseData);
+    } catch (error) {
+      res.status(500).json(error);
+    }
+  }
+);
+
+chatApiV1.get(
+  '/chat',
+  // ratelimiter
+  async (req: Request, res: Response, next: NextFunction) => {
+    const headers = {
+      'Content-Type': 'application/json; charset=utf-8'
+    };
+
+    try {
+      const entryKey = await req.body.entry_key;
+      const password = await req.body.password;
+
+      const isEntryKeyEmpty = checkIfStringIsEmpty(entryKey);
+      if (isEntryKeyEmpty) {
+        const statusCode = 401;
+        const errorObject = new ErrorObjectConstructor({
+          field: 'entry_key',
+          message: 'Entry key value is empty!',
+          name: 'Authentication error'
+        });
+        const responseData = new ResponseObject({
+          data: {},
+          errors: {
+            // @ts-ignore
+            ...errorObject.errors,
+            status: statusCode,
+            title: 'Cannot join to chat!'
+          }
+        });
+
+        res.set(headers);
+        res.status(statusCode);
+        res.json(responseData);
+        return;
+      }
+
+      const chat = await ChatModel.findOne({ entry_key: entryKey }).exec();
+      const wasChatFound = !!chat;
+
+      if (!wasChatFound) {
+        const statusCode = 401;
+        const errorObject = new ErrorObjectConstructor({
+          field: 'entry_key',
+          message: 'Chat not found!',
+          name: 'Authentication error'
+        });
+        const responseData = new ResponseObject({
+          data: {},
+          errors: {
+            // @ts-ignore
+            ...errorObject.errors,
+            status: statusCode,
+            title: 'Cannot join to chat!'
+          }
+        });
+
+        res.set(headers);
+        res.status(statusCode);
+        res.json(responseData);
+
+        return;
+      }
+
+      if (wasChatFound) {
+        // @ts-ignore
+        const doesChatHaveAPassword = chat.has_password;
+        // @ts-ignore
+        const isChatPasswordValid = chat.verifyPassword(password);
+
+        if (doesChatHaveAPassword) {
+          const isPasswordEmpty = checkIfStringIsEmpty(password);
+
+          if (isPasswordEmpty) {
+            const statusCode = 200;
+            const chatData = { has_password: chat?.has_password };
+            const responseData = new ResponseObject({
+              data: {
+                title: 'Joining the chat!',
+                attributes: {
+                  ...chatData
+                }
+              },
+              errors: {}
+            });
+
+            res.set(headers);
+            res.status(statusCode);
+            res.json(responseData);
+
+            return;
           }
 
-          const responseData = options;
-          res.json(responseData).status(200);
+          if (!isPasswordEmpty) {
+            if (!isChatPasswordValid) {
+              const statusCode = 401;
+              const errorObject = new ErrorObjectConstructor({
+                field: 'password',
+                message: 'Invalid password!',
+                name: 'Authentication error'
+              });
+              const responseData = new ResponseObject({
+                data: {},
+                errors: {
+                  // @ts-ignore
+                  ...errorObject.errors,
+                  status: statusCode,
+                  title: 'Cannot join to chat!'
+                }
+              });
+
+              res.set(headers);
+              res.status(statusCode);
+              res.json(responseData);
+
+              return;
+            }
+
+            if (isChatPasswordValid) {
+              const statusCode = 200;
+              const chatData = chat?.toJSON();
+              const responseData = new ResponseObject({
+                data: {
+                  title: 'Joining the chat!',
+                  attributes: {
+                    ...chatData
+                  }
+                },
+                errors: {}
+              });
+
+              res.set(headers);
+              res.status(statusCode);
+              res.json(responseData);
+
+              return;
+            }
+          }
         }
-      )(req, res, next);
+
+        if (!doesChatHaveAPassword) {
+          const statusCode = 200;
+          const chatData = chat?.toJSON();
+          const responseData = new ResponseObject({
+            data: {
+              title: 'Joining the chat!',
+              attributes: {
+                ...chatData
+              }
+            },
+            errors: {}
+          });
+
+          res.set(headers);
+          res.status(statusCode);
+          res.json(responseData);
+
+          return;
+        }
+      }
     } catch (error) {
-      res.json(error).status(500);
+      const errorObject = {
+        ...error.errors,
+        message: error.message
+      };
+
+      const statusCode = 500;
+      const responseData = new ResponseObject({
+        data: {},
+        errors: {
+          ...errorObject,
+          status: statusCode,
+          title: 'Something went wrong on our side!'
+        }
+      });
+
+      res.set(headers);
+      res.status(statusCode);
+      res.json(responseData);
+
+      return;
     }
   }
 );
