542f4824c86201419738e744e6ab61a55c29126f
funny family <maksvadik228@gmail.com>
2021-06-21 00:46:55 +0700

    update main file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { Server } from 'http';
+import path from 'path';
+
 import responseTime from 'response-time';
 import cors from 'cors';
-import { Server } from 'http';
+import cookieParser from 'cookie-parser';
 
 import dotenv from 'dotenv';
 dotenv.config();
+
+import { config } from './config';
 
 import { passport } from './middlewares/passport.middleware';
 
@@ -13,7 +18,7 @@ import { userApiV1 } from './modules/user.module';
 
 const app = express();
 const server = new Server(app);
-const port = process.env.PORT || 8800;
+const port = process.env.PORT || config.port;
 
 app.use(
   responseTime({
@@ -26,6 +31,7 @@ app.use(
     credentials: true
   })
 );
+app.use(cookieParser(config.secretOrKey));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
@@ -34,7 +40,8 @@ app.use(passport.session());
 app.use('/api/v1', chatApiV1);
 app.use('/api/v1', userApiV1);
 
+app.use('/', express.static(path.join(__dirname, './client/')));
+
 server.listen(port, () => {
   console.log('\x1b[36m', `Server running at: http://localhost:${port}/`);
-  // console.log(1213213);
 });
